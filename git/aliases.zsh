70732412dde070063c188c03ac4c0408f14ae9de
Raul Tammesoo <raultammesoo@gmail.com>
2022-11-20 21:17:36 +0200

    Add git alias for pulling dotfiles from upstream

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -25,3 +25,5 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+
+alias pulldots='git fetch upstream;git checkout raul;git merge upstream/raul'

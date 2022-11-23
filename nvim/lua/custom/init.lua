8bec3817260013beac67978ab3d3af285d211ed8
Sander Tammesoo <sander.tammesoo@gmail.com>
2022-11-23 21:47:35 +0200

    Turn on some options

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -1,20 +1,20 @@
--- local opt = vim.opt
--- local g = vim.g
+local opt = vim.opt
+local g = vim.g
 
--- g.loaded_netrw = 1
--- g.loaded_netrwPlugin = 1
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
 
--- -- opt.clipboard:append("unnamedplus")
--- opt.wrap = false
--- opt.autoindent = true
--- opt.relativenumber = true
--- -- opt.background = "dark"
--- -- opt.timeoutlen = 0
---
--- -- backspace
--- opt.backspace = "indent,eol,start"
--- opt.iskeyword:append "-"
--- opt.mousescroll = "ver:1,hor:1"
+-- opt.clipboard:append("unnamedplus")
+opt.wrap = false
+opt.autoindent = true
+opt.relativenumber = true
+-- opt.background = "dark"
+-- opt.timeoutlen = 0 -- Not currently working with which-key :/
+
+-- backspace
+opt.backspace = "indent,eol,start"
+opt.iskeyword:append "-"
+opt.mousescroll = "ver:1,hor:1"
 
 -- -- autocommand that reloads neovim and installs/updates/removes plugins
 -- -- when file is saved

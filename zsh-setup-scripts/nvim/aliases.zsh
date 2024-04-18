3577b9ea1c591d8e4a2ed5e35953c26847bea95f
SanderT <sander.tammesoo@gmail.com>
2024-04-18 23:51:06 +0300

    Added new nvim aliases

--- zsh-setup-scripts/nvim/aliases.zsh
+++ zsh-setup-scripts/nvim/aliases.zsh
@@ -2,3 +2,5 @@
 
 alias nvim-my="NVIM_APPNAME=nvim-myAstroNvim nvim"
 alias nvim-lazy="NVIM_APPNAME=nvim-LazyVim nvim"
+alias nvim-laravel="NVIM_APPNAME=nvim-Kauffinger-LazyVim-Laravel nvim"
+alias nvim-latex="NVIM_APPNAME=nvim-LaTeX nvim"

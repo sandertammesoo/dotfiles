71703d4b1a5e80e7fc36a813919c895aa29e3191
SanderT <sander.tammesoo@gmail.com>
2024-04-18 16:23:23 +0300

    Added lazyman to manage nvim configurations (not tested)

---
+++ zsh-setup-scripts/lazyman/install.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+set -e
+source helpers.zsh
+#
+# Lazyman
+#
+# https://lazyman.dev/about/
+#
+# The Lazyman project can be used to install, initialize, and manage multiple Neovim configurations.
+#
+
+if command -v lazyman >/dev/null; then
+  debug "lazyman already installed. Skipping..."
+else
+  # Check for lazyman submodule
+  if [[ -e $XDG_CONFIG_HOME/.dotfiles/git-submodules/nvim-lazyman/lazyman.sh ]]; then
+    source $XDG_CONFIG_HOME/.dotfiles/git-submodules/nvim-lazyman/lazyman.sh -h
+  fi
+fi
+
+

fd16a2f056e6f6367448cf6fb626ebb62b103fe9
SanderT <sander.tammesoo@gmail.com>
2024-08-07 00:23:51 +0300

    Set nvim-myAstroNvim as default NVIM_APPNAME

--- zsh-setup-scripts/nvim/aliases.zsh
+++ zsh-setup-scripts/nvim/aliases.zsh
@@ -4,3 +4,6 @@ alias nvim-my="NVIM_APPNAME=nvim-myAstroNvim nvim"
 alias nvim-lazy="NVIM_APPNAME=nvim-LazyVim nvim"
 alias nvim-laravel="NVIM_APPNAME=nvim-Kauffinger-LazyVim-Laravel nvim"
 alias nvim-latex="NVIM_APPNAME=nvim-LaTeX nvim"
+
+export NVIM_APPNAME=nvim-myAstroNvim
+
